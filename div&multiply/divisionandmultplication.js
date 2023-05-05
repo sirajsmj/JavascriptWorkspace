3d0e1bc8526bc492a72efe8e7af1d07d5e034ae0
sirajsmj <128237231+sirajsmj@users.noreply.github.com>
2023-05-05 16:36:22 +0530

    Add files via upload

---
+++ div&multiply/divisionandmultplication.js
@@ -0,0 +1,13 @@
+function multiply(){
+    let firstinputbox=document.getElementById("firstnum").value
+let secondinputbox=document.getElementById("secondnum").value
+let answer=firstinputbox*secondinputbox
+document.getElementById("result").innerHTML=answer
+}
+function divide(){
+    let firstinputbox=document.getElementById("firstnum").value
+let secondinputbox=document.getElementById("secondnum").value
+let answer=firstinputbox/secondinputbox
+document.getElementById("result").innerHTML=answer
+}
+
